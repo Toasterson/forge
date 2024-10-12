e45a3e193df2cb1867edde2db0b50bb3c6b25a27
Till Wegmueller <toasterson@gmail.com>
2024-10-12 16:19:11 +0200

    Add new test based on insta and only test forged
    Signed-off-by: Till Wegmueller <toasterson@gmail.com>

--- crates/forged/src/models/gates.rs
+++ crates/forged/src/models/gates.rs
@@ -1,8 +1,25 @@
-use super::_entities::gates::ActiveModel;
-use sea_orm::entity::prelude::*;
+use super::_entities::gates::{ActiveModel, Column, Entity, Model};
+use loco_rs::prelude::*;
 
 impl ActiveModelBehavior for ActiveModel {
     // extend activemodel below (keep comment for generators)
 }
 
-impl ActiveModel {}
+impl Model {
+    /// finds a gate by the provided name
+    ///
+    /// # Errors
+    ///
+    /// When could not find gate by the given name or DB query error
+    pub async fn find_by_name<S: AsRef<str>>(db: &DatabaseConnection, name: S) -> ModelResult<Self> {
+        let gate = Entity::find()
+            .filter(
+                query::condition()
+                    .eq(Column::Name, name.as_ref())
+                    .build(),
+            )
+            .one(db)
+            .await?;
+        gate.ok_or_else(|| ModelError::EntityNotFound)
+    }
+}
