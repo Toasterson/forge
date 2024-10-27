a390e38e1eadea9feed85215026fc47ef696b278
Till Wegmueller <toasterson@gmail.com>
2024-10-27 17:54:54 +0100

    CI Style fix
    Signed-off-by: Till Wegmueller <toasterson@gmail.com>

--- crates/forged/src/app.rs
+++ crates/forged/src/app.rs
@@ -52,9 +52,7 @@ impl Hooks for App {
 
     async fn connect_workers(ctx: &AppContext, queue: &Queue) -> Result<()> {
         queue
-            .register(crate::workers::report_worker::Worker::build(
-                ctx,
-            ))
+            .register(crate::workers::report_worker::Worker::build(ctx))
             .await?;
         Ok(())
     }
--- crates/forged/tests/workers/report_worker.rs
+++ crates/forged/tests/workers/report_worker.rs
@@ -12,10 +12,8 @@ async fn test_run_report_worker_worker() {
     let boot = testing::boot_test::<App>().await.unwrap();
 
     // Execute the worker ensuring that it operates in 'ForegroundBlocking' mode, which prevents the addition of your worker to the background
-    assert!(
-        Worker::perform_later(&boot.app_context, WorkerArgs {})
-            .await
-            .is_ok()
-    );
+    assert!(Worker::perform_later(&boot.app_context, WorkerArgs {})
+        .await
+        .is_ok());
     // Include additional assert validations after the execution of the worker
 }
